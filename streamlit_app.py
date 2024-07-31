0827e040bf20e92c4b12528184e5ab1936ab702d
Michelle Wee <michellew1822@gmail.com>
2024-07-31 12:14:11 -0700

    reading file

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,52 +19,44 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 
-# Call method to check for valid CSV file
 
-# Create a file uploader widget:
+# Create a file uploader widget and checking for valid file:
 
-fileName = st.file_uploader("Please input a csv file", type="csv")
+try:
 
-# Check if a file has been uploaded:
+    fileName = st.file_uploader("Please input a csv file", type="csv")
 
-if fileName is not None:
+    if fileName is None:
 
-    try:
+        rerunFile()
 
-        pass
-
-    except pd.errors.EmptyDataError:
+except pd.errors.EmptyDataError:
                 
-        st.error("The file is empty. Please try again.")
+    st.error("The file is empty. Please try again.")
 
-        # Rerun the app to clear error message
-
-        rerunFile()
-
-            
-    except pd.errors.ParserError:
-            
-        st.error("There was an error parsing the file. Please ensure the file is a valid CSV.")
-
-        # Rerun the app to clear error message
-
-        rerunFile()
-            
-    except Exception as e:
-            
-        # Displaying error message in the Streamlit app: 
-
-        st.error(f"An error occurred: {e}")
-
-        # Rerun the app to clear error message
-
-        rerunFile()
-           
-else:
-
-    # Rerun the app
+    # Rerun the app to clear error message
 
     rerunFile()
+
+            
+except pd.errors.ParserError:
+            
+    st.error("There was an error parsing the file. Please ensure the file is a valid CSV.")
+
+    # Rerun the app to clear error message
+
+    rerunFile()
+            
+except Exception as e:
+            
+     # Displaying error message in the Streamlit app: 
+
+    st.error(f"An error occurred: {e}")
+
+    # Rerun the app to clear error message
+
+    rerunFile()
+           
         
 # Read the CSV file into a DataFrame (CSV is in format of Pay Equity Instruction and Data Template)
 
