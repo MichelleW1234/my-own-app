93bfdcce53cc4cce0b62b9aed5d9b8e82485782c
Michelle Wee <michellew1822@gmail.com>
2024-07-31 11:59:59 -0700

    reading csv file

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,49 +3,72 @@ import pandas as pd
 
  # For running streamlit app through VSCode: streamlit run streamlit_app.py
 
-def mainMethod ():
+st.title("My new app")
+st.write(
+    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+)
 
-    st.title("My new app")
-    st.write(
-        "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-    )
+# Call method to check for valid CSV file
 
-    # Call method to check for valid CSV file
+# Create a file uploader widget:
 
-    fileName = fileChecking()
-        
-    # Read the CSV file into a DataFrame (CSV is in format of Pay Equity Instruction and Data Template)
+fileName = st.file_uploader("Please input a csv file", type="csv")
 
-    df = pd.read_csv(fileName)
+# Check if a file has been uploaded:
 
-    # Display the DataFrame
+if fileName is not None:
 
-    print(df)
+    try:
 
+        pass
 
+    except pd.errors.EmptyDataError:
+                
+        st.error("The file is empty. Please try again.")
 
-    # Prep information ()
+        # Rerun the app to clear error message
 
+        st.experimental_rerun() 
 
-    # Present it with a visual (create bar graph)
+            
+    except pd.errors.ParserError:
+            
+        st.error("There was an error parsing the file. Please ensure the file is a valid CSV.")
 
+        # Rerun the app to clear error message
 
+        st.experimental_rerun() 
+            
+    except Exception as e:
+            
+        # Displaying error message in the Streamlit app: 
 
+        st.error(f"An error occurred: {e}")
 
+        # Rerun the app to clear error message
 
-
-def fileChecking():
-
-    # Create a file uploader widget:
-    
-    fileName = st.file_uploader("Please input a csv file", type="csv")
-
-    # Check if a file has been uploaded:
-
-    if fileName is not None:
-
-        return fileName
+        st.experimental_rerun()
            
-    else:
+else:
 
-        fileChecking()
+    # Rerun the app
+
+    st.experimental_rerun() 
+        
+# Read the CSV file into a DataFrame (CSV is in format of Pay Equity Instruction and Data Template)
+
+df = pd.read_csv(fileName)
+
+# Display the DataFrame
+
+print(df)
+
+
+
+# Prep information ()
+
+
+# Present it with a visual (create bar graph)
+
+
+
